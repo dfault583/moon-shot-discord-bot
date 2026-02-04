bcadda693f2b3c4a5ccdfc02bfd552df960144ed
dfault583 <njsamples583@gmail.com>
2026-02-04 15:29:31 -0500

    Fix except indentation - add 4 spaces

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def make_chart(df, symbol, timeframe):
                                 mpf.plot(df, type='candle', style=s, volume=True, addplot=plots, savefig=dict(fname=buf, dpi=150, bbox_inches='tight'), title=f'{symbol} - {timeframe}')
                                 buf.seek(0)
                                 return buf
-    except:
+        except:
         return None
 @bot.command(name='c')
 async def chart_default(ctx, symbol: str = 'AAPL'):
